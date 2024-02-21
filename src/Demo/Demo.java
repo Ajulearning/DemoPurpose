87a9f296e49c8c9ced68fb3379d1fe4f35a93159
Ajulearning <unajai93@gmail.com>
2024-02-21 11:20:25 -0400

    New process

---
+++ src/Demo/Demo.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				System.out.println("Hello HALIFAX");
+				System.out.println("Welcome HALIFAX");
+				System.out.println("HAPPY SUNDAY");
+
+		
+		
+	}
+
+}
